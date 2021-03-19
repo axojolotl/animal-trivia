0daa6c845bae6042cf241906dd7b816a5a7d9921
wafforyu <wafforyu@gmail.com>
2021-03-20 02:24:46 +0800

    added files

---
+++ script.js
@@ -0,0 +1,37 @@
+function togglePopupTurtle(){
+  document.getElementById("popup-turtle").classList.toggle("active");
+}
+function togglePopupSnake(){
+  document.getElementById("popup-snake").classList.toggle("active");
+}
+function togglePopupSalamander(){
+  document.getElementById("popup-salamander").classList.toggle("active");
+}
+
+function togglePopupClown(){
+  document.getElementById("popup-clown").classList.toggle("active");
+}
+
+function togglePopupGold(){
+  document.getElementById("popup-gold").classList.toggle("active");
+}
+
+function togglePopupFrog(){
+  document.getElementById("popup-frog").classList.toggle("active");
+}
+
+function togglePopupParrot(){
+  document.getElementById("popup-parrot").classList.toggle("active");
+}
+function togglePopupOwl(){
+  document.getElementById("popup-owl").classList.toggle("active");
+}
+
+function togglePopupElephant(){
+  document.getElementById("popup-elephant").classList.toggle("active");
+}
+
+function togglePopupHorse(){
+  document.getElementById("popup-horse").classList.toggle("active");
+}
+
